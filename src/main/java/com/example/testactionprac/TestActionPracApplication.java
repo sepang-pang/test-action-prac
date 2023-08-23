36f6949c880be29eab5d830d1f95709cfbfd2ac0
오세창 <114675677+sepang-pang@users.noreply.github.com>
2023-08-23 09:58:10 +0900

    Initial commit

---
+++ src/main/java/com/example/testactionprac/TestActionPracApplication.java
@@ -0,0 +1,13 @@
+package com.example.testactionprac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestActionPracApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestActionPracApplication.class, args);
+    }
+
+}
