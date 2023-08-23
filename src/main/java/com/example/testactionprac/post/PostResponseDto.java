00c34408d09503339f2aa7f9fccc1108c39c9e42
오세창 <114675677+sepang-pang@users.noreply.github.com>
2023-08-23 11:13:07 +0900

    post-test

---
+++ src/main/java/com/example/testactionprac/post/PostResponseDto.java
@@ -0,0 +1,14 @@
+package com.example.testactionprac.post;
+
+import lombok.Getter;
+
+@Getter // 테스트용
+public class PostResponseDto {
+    private Long id;
+    private String content;
+
+    public PostResponseDto(Post post) {
+        this.id = post.getId();
+        this.content = post.getContent();
+    }
+}
